f51715921a0149bebc4ddfc5e71d9643c3f4afb3
HTM <haythamaaraba7@gmail.com>
2024-02-27 15:10:17 +0100

    updated terms and conditions styling

--- lib/views/TermsAndConditionsButton .dart
+++ lib/views/TermsAndConditionsButton .dart
@@ -6,87 +6,81 @@ class TermsAndConditionsButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black),
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 40),
-                const Text(
-                  "Welcome to Job Finder!",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              const Text(
+                "Welcome to Job Finder!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                const SizedBox(height: 10),
-                const Text(
-                  "By accessing or using our app (the \"Service\"), you agree to comply with and be bound by these Terms and Conditions. If you do not agree to these Terms, please do not use our Service.",
-                  style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "By accessing or using our app (the \"Service\"), you agree to comply with and be bound by these Terms and Conditions. If you do not agree to these Terms, please do not use our Service.",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              const SizedBox(height: 20),
+              buildSectionTitle("1. User Accounts"),
+              buildListItem(
+                  "- You must create an account to access certain features of the Service."),
+              buildListItem(
+                  "- Provide accurate, current, and complete information during the registration process."),
+              buildListItem(
+                  "- Keep your password secure and confidential. You are responsible for any activity on your account."),
+              const SizedBox(height: 20),
+              buildSectionTitle("2. Content"),
+              buildListItem(
+                  "- You are solely responsible for the content you post or submit on the Service."),
+              buildListItem(
+                  "- Do not post false, misleading, or inappropriate content."),
+              const SizedBox(height: 20),
+              buildSectionTitle("3. Privacy"),
+              buildListItem(
+                  "- Our Privacy Policy explains how we collect, use, and protect your personal information."),
+              buildSectionTitle("4. Job Listing"),
+              buildListItem(
+                  "- Job listings provided on the Service are for informational purposes only. - We do not guarantee that any job listing will be filled or that any candidate will be hired."),
+              buildSectionTitle("5. Prohibited Conduct"),
+              buildListItem(
+                  "- You agree not to engage in any of the following prohibited activities:"),
+              buildSectionTitle("6. Termination"),
+              buildListItem(
+                  "- We may terminate or suspend your account or ability to use the Service, in whole or in part, at our sole discretion, for any or no reason, and without notice or liability of any kind."),
+              buildSectionTitle(
+                  "7. Disclaimer of Warranty and Limitation of Liability"),
+              buildListItem(
+                  "- We make no warranties or representations about the Service or any content available on the Service."),
+              buildSectionTitle("8. Disclaimer of Warranties"),
+              buildListItem(
+                  "- We make no warranties or representations about the Service or any content available on the Service."),
+              buildSectionTitle("9. Indemnification"),
+              buildListItem(
+                  "- You agree to defend, indemnify, and hold us harmless from and against any claims, liabilities, damages, losses, and expenses, including, without limitation, reasonable legal and accounting fees, arising out of or in any way connected with your access to or use of the Service, or your violation of these Terms."),
+              buildSectionTitle("10. Changes to These Terms"),
+              buildListItem(
+                  "- We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion."),
+              const SizedBox(height: 20),
+              const Text(
+                "If you have any questions about these Terms and Conditions, please contact us at iotech.inovation@gmail.com.",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Thank you for using Job Finder!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                const SizedBox(height: 20),
-                buildSectionTitle("1. User Accounts"),
-                buildListItem(
-                    "- You must create an account to access certain features of the Service."),
-                buildListItem(
-                    "- Provide accurate, current, and complete information during the registration process."),
-                buildListItem(
-                    "- Keep your password secure and confidential. You are responsible for any activity on your account."),
-                const SizedBox(height: 20),
-                buildSectionTitle("2. Content"),
-                buildListItem(
-                    "- You are solely responsible for the content you post or submit on the Service."),
-                buildListItem(
-                    "- Do not post false, misleading, or inappropriate content."),
-                const SizedBox(height: 20),
-                buildSectionTitle("3. Privacy"),
-                buildListItem(
-                    "- Our Privacy Policy explains how we collect, use, and protect your personal information."),
-                buildSectionTitle("4. Job Listing"),
-                buildListItem(
-                    "- Job listings provided on the Service are for informational purposes only. - We do not guarantee that any job listing will be filled or that any candidate will be hired."),
-                buildSectionTitle("5. Prohibited Conduct"),
-                buildListItem(
-                    "- You agree not to engage in any of the following prohibited activities:"),
-                buildSectionTitle("6. Termination"),
-                buildListItem(
-                    "- We may terminate or suspend your account or ability to use the Service, in whole or in part, at our sole discretion, for any or no reason, and without notice or liability of any kind."),
-                buildSectionTitle(
-                    "7. Disclaimer of Warranty and Limitation of Liability"),
-                buildListItem(
-                    "- We make no warranties or representations about the Service or any content available on the Service."),
-                buildSectionTitle("8. Disclaimer of Warranties"),
-                buildListItem(
-                    "- We make no warranties or representations about the Service or any content available on the Service."),
-                buildSectionTitle("9. Indemnification"),
-                buildListItem(
-                    "- You agree to defend, indemnify, and hold us harmless from and against any claims, liabilities, damages, losses, and expenses, including, without limitation, reasonable legal and accounting fees, arising out of or in any way connected with your access to or use of the Service, or your violation of these Terms."),
-                buildSectionTitle("10. Changes to These Terms"),
-                buildListItem(
-                    "- We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion."),
-                const SizedBox(height: 20),
-                const Text(
-                  "If you have any questions about these Terms and Conditions, please contact us at iotech.inovation@gmail.com.",
-                  style: TextStyle(color: Colors.black, fontSize: 14),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Thank you for using Job Finder!",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
